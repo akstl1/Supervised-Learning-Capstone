9b9f8449c8ae27b9bb46f55a576090346243ced7
Allan Khariton <allankhariton@gmail.com>
2023-10-24 22:39:37 -0400

    added py and joblib files

--- Streamlit/churn.py
+++ Streamlit/churn.py
@@ -8,6 +8,7 @@ Created on Sun Oct 22 23:15:21 2023
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
@@ -23,32 +24,36 @@ from sklearn.linear_model import LogisticRegression
 import joblib
 
 
-data = pd.read_csv('parkinsons.data')
+df = pd.read_csv('DATA/Telco-Customer-Churn.csv')
+df.drop('customerID', axis=1, inplace=True)
 
-data.drop('name',axis=1,inplace=True)
+def tenure_cohort(tenure):
+    if tenure<13:
+        return '0-12 months'
+    elif tenure<25:
+        return '12-24 months'
+    elif tenure<49:
+        return '24-48 months'
+    return 'over 48 months' 
 
-data.head()
+df['tenure_cohort'] = df['tenure'].apply(tenure_cohort)
+X=df.drop('Churn', axis=1)
+y=df['Churn']
+X = pd.get_dummies(X,drop_first=True)
 
-data.info()
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-data = shuffle(data,random_state=42)
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, plot_confusion_matrix,classification_report
 
-data['status'].value_counts(normalize=True) 
+rf_model = RandomForestClassifier()
+param_grid = {'n_estimators':[50,100], 'max_depth': [2,4,6,8,10]}
+grid = GridSearchCV(rf_model,param_grid)
+grid.fit(X_train, y_train)
+grid.best_params_
 
-X=data.drop('status',axis=1)
-y=data['status']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-scaler = MinMaxScaler()
-
-X_train_lr = pd.DataFrame(scaler.fit_transform(X_train),columns=X_train.columns) #return as df
-X_test_lr = pd.DataFrame(scaler.transform(X_test),columns=X_test.columns) #return as df
-
-clf=RandomForestClassifier(random_state=42,n_estimators=500,max_features=5)
-clf.fit(X_train,y_train)
-y_pred_class = clf.predict(X_test)
-y_pred_proba = clf.predict_proba(X_test)[:,1]
-
-joblib.dump(clf, "parkinsons_model.joblib")
+joblib.dump(grid, "churn_model.joblib")
 
